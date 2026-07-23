efaf659434c1a541e99e796d8ff68858ff6818e4
Minakshi Kaushik <minakshikaushik22122005@gmail.com>
2026-07-23 14:13:07 +0530

    update requirements.tx and streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import os
 import tempfile
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Optional
 
 import cv2
 import numpy as np
 import streamlit as st
-from PIL import Image
+
 from ultralytics import YOLO
 
 from inference.detect_image import ImageDetector
